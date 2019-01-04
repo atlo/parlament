9540d4e4dec52656bac19c9abdf408d9dcdec300
László Horváth <bedafoltyn@gmail.com>
2019-01-04 14:06:28 +0100

    arrows added to slider-2, only shown in mobile view

--- javascripts/script.js
+++ javascripts/script.js
@@ -1,10 +1,11 @@
 $('.slider-1').slick({
   slidesToShow: 1,
   slidesToScroll: 1,
-  arrows: false,
   fade: true,
   asNavFor: '.slider-2',
   infinite: false,
+  nextArrow: '<button type="button" class="slick-next">tovább</button>',
+  prevArrow: '<button type="button" class="slick-prev">vissza</button>',
   adaptiveHeight: true
 })
 
